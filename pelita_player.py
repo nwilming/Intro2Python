bd54c5290b80d749ccd37607c39ca7fd9abb35b5
Niklas Wilming <niklaswilming@fastmail.fm>
2018-05-15 08:30:56 +0200

    Simplify TSP pelita player

--- pelita_player.py
+++ pelita_player.py
@@ -4,97 +4,95 @@ import networkx as nx
 
 from tsp_solver.greedy import solve_tsp
 
-goals = None
 
+class Collector(AbstractPlayer):
+    """
+    A Player that aims to collect as much as possible. It
+    coordinates with other Collector players.
+    """
 
-class Goals(object):
-    '''
-    Represent and coordinate goals for two bots.
-    '''
+    def set_initial(self):
+        '''
+        Precompute a graph of the maze, an adjacency list that contains
+        distances between food items and an optimal tour through food.
 
-    def __init__(self, bot, maze, universe):
-        self.graph = nx.Graph()
+        '''
+        universe = self.current_uni
+        self.graph = nx.Graph()  # Represent board as a graph
         # now iterate over all free positions
         for pos, neighbors in universe.free_positions():
             for n in neighbors:
                 self.graph.add_edge(pos, n)
 
-        n_food = len(bot.enemy_food)
-        self.adjacency = np.zeros((n_food, n_food))
-        for i, f1 in enumerate(bot.enemy_food):
-            for j, f2 in enumerate(bot.enemy_food):
-                self.adjacency[i, j] = len(
-                    nx.shortest_path(self.graph, f1, f2))
-        self.targets = bot.enemy_food
-        self.target_idx = np.arange(len(self.targets))
-        self.shortest_tour = self.targets
-        self.lowest_cost = 10000000
-        self.opt_tours(2)
+        self.shortest_tour = self.enemy_food
+        self.opt_tours()
 
-    def update(self, bot):
+    def get_move(self):
         '''
         Compute a new move for this bot.
         '''
-        enemy_pos = [e.current_pos for e in bot.enemy_bots]
+        enemy_pos = [e.current_pos for e in self.enemy_bots]
         try:
-            if bot.me.index == 0:
-                target = self.shortest_tour[0]
-            else:
-                target = self.shortest_tour[-1]
-            path = nx.shortest_path(self.graph, bot.current_pos, target)
-            legal_moves = bot.legal_moves
+            index = 0 if self.me.index == 0 else -1
+            target = self.shortest_tour[index]
+            if target == self.current_pos:
+                self.opt_tours()
+                target = self.shortest_tour[index]
+
+            # Compute shortest path to next target
+            path = nx.shortest_path(self.graph, self.current_pos, target)
+            # Translate next position in path to target to a move
             pos = path[1]
-            pos2move = dict((v, k) for k, v in legal_moves.items())
+            pos2move = dict((v, k) for k, v in self.legal_moves.items())
             move = pos2move[pos]
+
             if pos in enemy_pos:
-                print('DANGER')
+                # Next move moves onto enemy bot, return random
+                # TODO: Check if in own half (will wrongly avoid in this case)
+                print('Too close to enemy bot')
                 del pos2move[pos]
-                bot.rnd.choice(list(pos2move.values()))
-            if pos in bot.enemy_food:
-                self.targets = [x for x in self.targets if not (x == pos)]
+                return self.rnd.choice(list(pos2move.values()))
+            if pos in self.enemy_food:
+                # If next move is onto food pellet remove it from targets
                 self.shortest_tour = [
                     x for x in self.shortest_tour if not (x == pos)]
 
             return move
+
         except (KeyError, IndexError):
-            print('RANDOM')
-            return bot.rnd.choice(list(legal_moves.keys()))
+            import traceback
+            print(traceback.format_exc())
+            return self.rnd.choice(list(self.legal_moves.keys()))
 
     def tour_length(self, tour):
         '''
-        Compute the length of a tour
+        Compute the length of a tour. 
+
+        This can be ommitted when using a proper TSP library.
         '''
         cost = 0
         for start, end in zip(tour[0:-1], tour[1:]):
             cost += self.adjacency[start, end]
         return cost
 
-    def opt_tours(self, dt):
+    def opt_tours(self):
         '''
-        Generate optimal tour.
+        Generate shortest tour through enemy food.
         '''
+        n_food = len(self.enemy_food)
+        # Create adjacency list of distance of enemy food
+        self.adjacency = np.zeros((n_food, n_food))
+        for i, f1 in enumerate(self.enemy_food):
+            for j, f2 in enumerate(self.enemy_food):
+                self.adjacency[i, j] = len(
+                    nx.shortest_path(self.graph, f1, f2))  # Compute shortest path
         tour = solve_tsp(self.adjacency)
-        cost = self.tour_length(tour)
-        if cost < self.lowest_cost:
-            print(cost)
-            self.lowest_cost = cost
-            self.shortest_tour = [self.targets[t] for t in tour]
+        self.shortest_tour = [self.enemy_food[t] for t in tour]
 
 
-class Collector(AbstractPlayer):
-    """
-    A Player aims to collect as much as possible. It
-    coordinates with other Collector players.
-    """
-
-    def set_initial(self):
-        global goals
-        if goals is None:
-            goals = Goals(self, self.current_uni.maze, self.current_uni)
-
-    def get_move(self):        
-        return goals.update(self)
-
-
-def team():
+def team1():
     return SimpleTeam("CollectorZ", Collector(), Collector())
+
+
+def team2():
+    return SimpleTeam("KolleKtorZ", Collector(), Collector())
